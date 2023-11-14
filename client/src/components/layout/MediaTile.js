0b4fe90e9a81715b2a902d048e01f3f0214ca544
Tracy Campbell <tracy.cmpbll@gmail.com>
2023-11-14 18:02:07 -0500

    removed classNames (styling tk)

--- client/src/components/layout/MediaTile.js
+++ client/src/components/layout/MediaTile.js
@@ -4,9 +4,9 @@ const MediaTile = ({film}) => {
     const { id, title, cover_image } = film
 
     return (
-        <div className="media-tile">
-            <img src={cover_image} className="media-tile-cover"/>
-            <p className="media-tile-title">{title}</p>
+        <div>
+            <img src={cover_image} />
+            <p>{title}</p>
         </div>
     )
 } 
