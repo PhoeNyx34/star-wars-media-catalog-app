3a83994f708ff86b257378946ae40a33bc20b952
Tracy Campbell <tracy.cmpbll@gmail.com>
2023-11-20 13:20:04 -0500

    separated deleteMedia function into services file

--- client/src/components/layout/MediaTile.js
+++ client/src/components/layout/MediaTile.js
@@ -1,52 +1,32 @@
 import React from "react"
 import { Link } from "react-router-dom"
 
+import executeDelete from "../../services/deleteMedia.js"
+
 const MediaTile = ({ item, user }) => {
     const { id, title, cover_image } = item
 
-    if (!user) {
-        return (
-            <Link to={`/${id}`} className="cell small-3 media-tile">
-                <img src={cover_image} className="media-tile-cover"/>
-                <p className="media-tile-title">{title}</p>
-            </Link> 
-        )   
-    }
-
-    if (user.type === "member") {
-        return (
-            <Link to={`/${id}`} className="cell small-3 media-tile">
-                <img src={cover_image} className="media-tile-cover"/>
-                <p className="media-tile-title">{title}</p>
-            </Link>    
-        )    
-    }
-
-    const deleteMedia = async (event) => {
+    const deleteMedia = (event) => {
         event.preventDefault()
-        try {
-            const response = await fetch(`/api/v1/media/${id}`, {method: "DELETE"})
-            if (!response.ok) {
-                const errorMessage = `${response.status} (${response.statusText})`
-                const error = new Error(errorMessage)
-                throw error
-            }
-            location.href = "/"
-        } catch (error) {
-            window.alert("Delete unsuccessful")
-            console.error(`Error in fetch: ${error.message}`)
-        }
+        executeDelete(id)
     }
 
-    if (user.type === "admin") {
-        return (
-            <Link to={`/${id}`} className="cell small-3 media-tile">
-                <img src={cover_image} className="media-tile-cover"/>
-                <p className="media-tile-title">{title}</p>
+    let adminButtons
+    if (user && user.type === "admin") {
+        adminButtons = (
+            <div className="tile-admin-buttons">
                 <button className="button" onClick={deleteMedia}>Delete</button>
-            </Link>
+            </div>
         )
     }
+
+    return (
+        <Link to={`/${id}`} className="cell small-3 media-tile">
+            <img src={cover_image} className="media-tile-cover"/>
+            <p className="media-tile-title">{title}</p>
+            {adminButtons}
+        </Link> 
+    )  
 } 
 
 export default MediaTile
